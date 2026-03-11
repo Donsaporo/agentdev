d777779786c9a2a1b8e6aa276031b39ce9a9a1bb
Donsaporo <jp14carbonell@gmail.com>
2026-03-11 03:07:59 -0500

    Updated index.ts

--- supabase/functions/whatsapp-send-message/index.ts
+++ supabase/functions/whatsapp-send-message/index.ts
@@ -295,6 +295,7 @@ Deno.serve(async (req: Request) => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
+          messaging_product: "whatsapp",
           code_method: codeMethod,
           language: codeLang,
         }),
